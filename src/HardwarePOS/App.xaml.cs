53d15c5b912cabecdc2f1d9a8ae4f6daa1af5c8e
ceto-31 <anicetoakaajobina@gmail.com>
2026-07-28 14:38:34 +0800

    Configure LiveCharts on app startup.

--- src/HardwarePOS/App.xaml.cs
+++ src/HardwarePOS/App.xaml.cs
@@ -1,7 +1,16 @@
 ﻿using System.Windows;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using SkiaSharp;
 
 namespace HardwarePOS;
 
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        LiveCharts.Configure(config => config.AddSkiaSharp());
+        LiveChartsSkiaSharp.DefaultSKTypeface = SKTypeface.FromFamilyName("Segoe UI");
+        base.OnStartup(e);
+    }
 }
